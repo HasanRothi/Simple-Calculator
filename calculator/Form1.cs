aa40378469137b0136a51662ef757cd85bdb77e1
hasanrothi <hasanrothi99@gmail.com>
2020-07-05 10:05:46 +0600

    V1

---
+++ calculator/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        double value;
+        string operation;
+        bool opertation_press;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            if((result.Text=="0") || (opertation_press))
+            {
+                result.Clear();
+               // result.Text = "";
+            }
+            opertation_press = false;
+            Button b = (Button)sender;
+            result.Text += b.Text;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = double.Parse(result.Text);
+            opertation_press = true;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+            switch (operation)
+            {
+                case "+":
+                    result.Text = (value + double.Parse(result.Text)).ToString();
+                    break;
+                case "-":
+                    result.Text = (value - double.Parse(result.Text)).ToString();
+                    break;
+                case "*":
+                    result.Text = (value * double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                    result.Text = (value / double.Parse(result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            opertation_press = false;
+
+        }
+
+    }
+}
